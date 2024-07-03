c92f5d4104268b76a0427cfe7aacff8fd1bc3e76
Han <68423335+Han-Too@users.noreply.github.com>
2024-07-03 13:13:36 +0700

    remove button api doc

--- protected/resources/views/admin/partials/sidebar.blade.php
+++ protected/resources/views/admin/partials/sidebar.blade.php
@@ -698,7 +698,7 @@
     </div>
     <!--end::Aside menu-->
     <!--begin::Footer-->
-    <div class="aside-footer flex-column-auto py-5" id="kt_aside_footer">
+    {{-- <div class="aside-footer flex-column-auto py-5" id="kt_aside_footer">
         <a href="../../demo8/dist/documentation/getting-started.html" class="btn btn-custom btn-primary w-100"
             data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click"
             title="200+ in-house components and 3rd-party plugins">
@@ -718,6 +718,6 @@
             </span>
             <!--end::Svg Icon-->
         </a>
-    </div>
+    </div> --}}
     <!--end::Footer-->
 </div>
